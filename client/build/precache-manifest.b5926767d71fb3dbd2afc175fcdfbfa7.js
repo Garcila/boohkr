a75dac4194eb4c9e2df9cca5bd223a9dbdf4f4fb
German Arcila <germanarcila@gmail.com>
2019-04-24 12:49:34 -0400

    add build

---
+++ client/build/precache-manifest.b5926767d71fb3dbd2afc175fcdfbfa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0653d51590a4e0ab8456",
+    "url": "/static/js/main.cf553b37.chunk.js"
+  },
+  {
+    "revision": "bfd2f3c50a1b65413de4",
+    "url": "/static/js/2.22fe22d2.chunk.js"
+  },
+  {
+    "revision": "0653d51590a4e0ab8456",
+    "url": "/static/css/main.1cf26733.chunk.css"
+  },
+  {
+    "revision": "bfd2f3c50a1b65413de4",
+    "url": "/static/css/2.23a4dcdc.chunk.css"
+  },
+  {
+    "revision": "3f5c0d20202cfd22d4f353b9223510e6",
+    "url": "/index.html"
+  }
+];
